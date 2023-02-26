3ae9d58a7a893662b644cb0ec0337d57ac9ec4d9
Blane Cummings <blanecummings@hotmail.com>
2023-02-26 10:56:19 -0600

    code snippets/autocompletion

--- nvim/lua/bcummings/plugins-setup.lua
+++ nvim/lua/bcummings/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer()
 
@@ -20,43 +20,46 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+	return
 end
 
 return packer.startup(function(use)
-    use("wbthomason/packer.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("folke/tokyonight.nvim")
-    use("christoomey/vim-tmux-navigator")
-    use("nvim-tree/nvim-tree.lua")
-    use("nvim-tree/nvim-web-devicons")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("jayp0521/mason-null-ls.nvim")
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/cmp-nvim-lsp")
-    use({ "glepnir/lspsaga.nvim", branch = "main" })
-    use("onsails/lspkind.nvim")
-    use("numToStr/Comment.nvim")
-    use("szw/vim-maximizer")
-    use("tpope/vim-surround")
-    use("inkarkat/vim-ReplaceWithRegister")
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("folke/tokyonight.nvim")
+	use("christoomey/vim-tmux-navigator")
+	use("nvim-tree/nvim-tree.lua")
+	use("nvim-tree/nvim-web-devicons")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+	use("onsails/lspkind.nvim")
+	use("numToStr/Comment.nvim")
+	use("szw/vim-maximizer")
+	use("tpope/vim-surround")
+	use("inkarkat/vim-ReplaceWithRegister")
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = function()
-            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-            ts_update()
-        end
-    })
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
