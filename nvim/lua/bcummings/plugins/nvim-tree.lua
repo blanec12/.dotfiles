2ff89a35c9c264f8d03c9c7d15bfc04785381757
Blane Cummings <blanecummings@hotmail.com>
2023-11-26 17:33:57 -0600

    rework nvim config so I can better understand it

--- nvim/lua/bcummings/plugins/nvim-tree.lua
+++ nvim/lua/bcummings/plugins/nvim-tree.lua
@@ -1,15 +1,45 @@
-local setup, nvimtree = pcall(require, "nvim-tree")
-if not setup then 
-    print("nvim-tree is fucked")
-end
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-nvimtree.setup({
-    actions = {
-        open_file = {
-            window_picker = { enable = false },
-        },
-    },
-})
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "",
+							arrow_open = "",
+						},
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+	end,
+}
--- nvim/lua/bcummings/plugins/telescope.lua
+++ nvim/lua/bcummings/plugins/telescope.lua
@@ -1,23 +1,33 @@
-local telescope_setup, telescope = pcall(require, "telescope")
-if not telescope_setup then
-    return
-end
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-local actions_setup, actions = pcall(require, "telescope.actions")
-if not actions_setup then
-    return
-end
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k"] = actions.move_selection_previous,
+						["<C-j"] = actions.move_selection_next,
+						["<C-q"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-telescope.setup({
-    defaults = {
-        mappings = {
-            i = {
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist
-            }
-        },
-    },
-})
+		telescope.load_extension("fzf")
 
-telescope.load_extension("fzf")
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
+		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+	end,
+}
