709ab74fdfd2af0c62404dc0fa6fb4e553a701c1
Blane Cummings <blanecummings@hotmail.com>
2025-11-10 11:51:05 -0600

    add install script for zsh

---
+++ scripts/install-zsh.sh
@@ -0,0 +1,13 @@
+!#/usr/bin/env bash
+set -e
+
+if ! command -v zsh &>/dev/null; then
+    sudo apt update
+    sudo apt install -y zsh
+fi
+
+if ["$SHELL" != "$(which zsh)"]; then
+    chsh -s "$(which zsh)"
+fi
+
+# TODO: Auto install oh-my-zsh with defaults
